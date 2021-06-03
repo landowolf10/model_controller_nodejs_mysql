df7c75f69f1babe0ca025f957bb99f4512a3d660
Orlando Avila <oa1220@dexcom.com>
2021-06-03 17:19:25 -0500

    Get notes method changed from post to get.

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,6 +1,8 @@
 module.exports = {
-    HOST: "localhost",
-    USER: "root",
-    PASSWORD: "landowolf10",
-    DB: "company"
-  };
+  HOST: "localhost",
+  USER: "root",
+  PASSWORD: "landowolf10",
+  DB: "notes"
+};
+
+//b931203ef9d942:9461bf0c@us-cdbr-east-03.cleardb.com/heroku_fee189397f09cd4
